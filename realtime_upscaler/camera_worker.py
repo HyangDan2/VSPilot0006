54b8b86a3f21200f7f662ab9d10a07d261715a78
HyangDan2 <49368735+HyangDan2@users.noreply.github.com>
2025-07-31 09:09:21 +0900

    Create camera_worker.py

---
+++ realtime_upscaler/camera_worker.py
@@ -0,0 +1,15 @@
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+def get_camera_frame():
+    if not cap.isOpened():
+        return None
+    ret, frame = cap.read()
+    if not ret:
+        return None
+    return frame
+
+def release_camera():
+    if cap.isOpened():
+        cap.release()
